db7b7e3195f598b561e00aca00fc77c5b678da41
Mwadali <wellingtonmwadali@gmail.com>
2023-02-16 14:05:18 -0800

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 		long long int d;
 		float f;
 
-	printf("size of char:%lu byte(s)\n", (unsigned long)Sizeof(a));
-	printf("size of int:%lu byte(s)\n", (unsigned long)Sizeof(b));
-	printf("size of long int:%lu byte(s)\n", (unsigned long)Sizeof(c));
-	printf("size of long long int:%lu byte(s)\n", (unsigned long)Sizeof(d));
-	printf("size of float:%lu byte(s)\n", (unsigned long)Sizeof(f));
+	printf("Size of char:%lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of int:%lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long int:%lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of long long int:%lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of float:%lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 	}
