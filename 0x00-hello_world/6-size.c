75b5f2459ce6956a44161eff8cd8269ed1847f87
Mwadali <wellingtonmwadali@gmail.com>
2023-02-16 13:58:57 -0800

    advanced tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 		long long int d;
 		float f;
 
-	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("size of char:%lu byte(s)\n", (unsigned long)Sizeof(a));
+	printf("size of int:%lu byte(s)\n", (unsigned long)Sizeof(b));
+	printf("size of long int:%lu byte(s)\n", (unsigned long)Sizeof(c));
+	printf("size of long long int:%lu byte(s)\n", (unsigned long)Sizeof(d));
+	printf("size of float:%lu byte(s)\n", (unsigned long)Sizeof(f));
 	return (0);
 	}
