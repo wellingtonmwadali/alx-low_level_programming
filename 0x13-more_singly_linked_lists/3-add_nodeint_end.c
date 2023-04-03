6d1f3968f4f0898cc8ed32ecaf630f16ed26704c
Mwadali <wellingtonmwadali@gmail.com>
2023-04-03 13:00:38 -0700

    task 3 cor

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *newnode;
 	listint_t *temp = *head;
 
-	newnode = malloc(sizeof(listint_t);
+	newnode = malloc(sizeof(listint_t));
 
 			if (!newnode)
 			return (NULL);
