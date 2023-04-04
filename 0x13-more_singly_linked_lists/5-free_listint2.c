e1cff2b0a998835d65cabf7930d0d8e69155a5a7
Mwadali <wellingtonmwadali@gmail.com>
2023-04-04 05:43:04 -0700

    task 5 cor

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,14 +5,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp
+	listint_t *temp;
 
 		if (head == NULL)
 			return;
+
 	while (*head)
 	{
 		temp = (*head)->next;
-		free(*head)
+		free (*head)
 		* head = temp;
 	}
 	*head = NULL;
