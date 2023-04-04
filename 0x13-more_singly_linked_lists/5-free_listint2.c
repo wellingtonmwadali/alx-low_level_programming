63577327c41f87cb078d9df74abe77d6640a724c
Mwadali <wellingtonmwadali@gmail.com>
2023-04-04 05:23:49 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2(listint_t **head)
+ * @head: pointer to the first node in a linked list
+ * 
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp
+
+		if (head == NULL)
+			return;
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head)
+			*head = temp;
+	}
+
+}
