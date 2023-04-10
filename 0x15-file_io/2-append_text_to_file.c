88e72e5dbbc6fea1421bf8aadd152234d9818754
Mwadali <wellingtonmwadali@gmail.com>
2023-04-10 06:13:00 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check how the code is
+ * @ac: 1st parameter
+ * @av: 2nd parameter
+ * Return: always 0
+ */
+int main(int ac, char **av)
+{
+	int abc;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text \n", av[0]);
+		exit(1);
+	}
+	abc = append_text_to_file(av[1], av[2]);
+	printf("- %i)\n", abc);
+	return (0);
+}
