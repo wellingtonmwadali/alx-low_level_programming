ce304c97c3ce8afc1d26be882b252432dc341471
Mwadali <wellingtonmwadali@gmail.com>
2023-04-10 05:40:04 -0700

    task 0 cor

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,9 +3,9 @@
  * read_textfile - reads a text file nd prints it to the POSIX stdout
  * @filename: it is the name of the file that is being read
  * @letters: number of letters thst should be read and printed
- * Return: the actual number of letters read and printed, or 0 incase of failure
+ * Return: the actual number of letters read and printed, or 0 if fail
  */
-ssize_t read_textfile(const chr *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t a, b;
 	int abc;
@@ -13,12 +13,13 @@ ssize_t read_textfile(const chr *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	abc - open(filename, O_RDONLY);
+	abc = open(filename, O_RDONLY);
 	if (abc == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	a = read(abc, buffer, letters);
 	b = write(STDOUT_FILENO, buffer, a);
-		free (buffer);
-		close (abc);
+		free(buffer);
+		close(abc);
 		return (b);
+}
