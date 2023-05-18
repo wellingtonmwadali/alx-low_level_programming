f11312e326d4233c0f727e45f90c0e8748bb8a6c
Mwadali <wellingtonmwadali@gmail.com>
2023-05-18 09:25:52 -0700

    7-insert_dnodeint.c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,8 @@
  * @val: value of the new node
  * Return: the address of the new node, or NULL if it failed
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int val)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head,
+		unsigned int index, int val)
 {
 	dlistint_t *newNode;
 	dlistint_t *current;
